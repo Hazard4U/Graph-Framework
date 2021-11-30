2947daf36d3a9d4be10cdd1c073a69c3c3cf66c3
Théo Martel <martel.theo@outlook.com>
2021-11-30 19:57:07 +0100

    dijkstra

--- src/main/java/AdjacencyList/UndirectedValuedGraph.java
+++ src/main/java/AdjacencyList/UndirectedValuedGraph.java
@@ -1,29 +1,31 @@
 package AdjacencyList;
 
-import java.util.ArrayList;
-
 import GraphAlgorithms.GraphTools;
 import Nodes.UndirectedNode;
 
-public class UndirectedValuedGraph extends UndirectedGraph{
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 
-	//--------------------------------------------------
+public class UndirectedValuedGraph extends UndirectedGraph {
+
+    //--------------------------------------------------
     // 				Constructors
     //--------------------------------------------------
 
     public UndirectedValuedGraph(int[][] matrixVal) {
-    	super();
-    	this.order = matrixVal.length;
+        super();
+        this.order = matrixVal.length;
         this.nodes = new ArrayList<>();
         for (int i = 0; i < this.order; i++) {
             this.nodes.add(i, this.makeNode(i));
         }
         for (UndirectedNode n : this.getNodes()) {
             for (int j = n.getLabel(); j < matrixVal[n.getLabel()].length; j++) {
-            	UndirectedNode nn = this.getNodes().get(j);
+                UndirectedNode nn = this.getNodes().get(j);
                 if (matrixVal[n.getLabel()][j] != 0) {
-                    n.getNeighbours().put(nn,matrixVal[n.getLabel()][j]);
-                    nn.getNeighbours().put(n,matrixVal[n.getLabel()][j]);
+                    n.getNeighbours().put(nn, matrixVal[n.getLabel()][j]);
+                    nn.getNeighbours().put(n, matrixVal[n.getLabel()][j]);
                     this.m++;
                 }
             }
@@ -33,14 +35,14 @@ public class UndirectedValuedGraph extends UndirectedGraph{
     //--------------------------------------------------
     // 				Methods
     //--------------------------------------------------
-    
+
 
     /**
      * Adds the edge (from,to) with cost if it is not already present in the graph
      */
     public void addEdge(UndirectedNode x, UndirectedNode y, int cost) {
-    	x.addNeigh(y, cost);
-    	y.addNeigh(x, cost);
+        x.addNeigh(y, cost);
+        y.addNeigh(x, cost);
     }
 
     @Override
@@ -56,7 +58,7 @@ public class UndirectedValuedGraph extends UndirectedGraph{
         s.append("\n");
         return s.toString();
     }
-    
+
     public static void main(String[] args) {
         int[][] matrix = GraphTools.generateGraphData(10, 15, false, true, false, 100001);
         int[][] matrixValued = GraphTools.generateValuedGraphData(10, false, true, true, false, 100001);
@@ -68,5 +70,38 @@ public class UndirectedValuedGraph extends UndirectedGraph{
         al.addEdge(al.getNodes().get(0), al.getNodes().get(1), 16);
         System.out.println(al.isEdge(al.getNodes().get(0), al.getNodes().get(1)));
         // A completer
+        System.out.println(al);
+        al.dijkstra();
+    }
+
+    public void dijkstra() {
+        LinkedList<UndirectedNode> nonParcouru = new LinkedList<>(getNodes());
+        HashMap<Integer, Integer> values = new HashMap<>();
+        HashMap<UndirectedNode, UndirectedNode> predecesseur = new HashMap<>();
+
+        UndirectedNode currentNode;
+        values.put(getNodes().get(0).getLabel(), 0);
+
+        while (!nonParcouru.isEmpty()) {
+            int min = Integer.MAX_VALUE;
+            currentNode = nonParcouru.get(0);
+            for (UndirectedNode node : nonParcouru) {
+                if (nonParcouru.contains(node) && values.containsKey(node.getLabel()) && values.get(node.getLabel()) < min) {
+                    currentNode = node;
+                    min = values.get(node.getLabel());
+                }
+            }
+            nonParcouru.remove(currentNode);
+            if (min < Integer.MAX_VALUE) {
+                for (UndirectedNode node : currentNode.getNeighbours().keySet()) {
+                    if (nonParcouru.contains(node) && (!values.containsKey(node.getLabel()) || min + currentNode.getNeighbours().get(node) < values.get(node.getLabel()))) {
+                        values.put(node.getLabel(), min + currentNode.getNeighbours().get(node));
+                        predecesseur.put(node, currentNode);
+                    }
+                }
+            }
+        }
+        System.out.println(values);
+        System.out.println(predecesseur);
     }
 }
